f995f4e9d253e9185c0adb74a1d0bb8dc01bd8be
En1Li <1401207141@qq.com>
2022-08-02 12:17:03 +0800

    完成权限控制

---
+++ src/permission.js
@@ -0,0 +1,22 @@
+import router from '@/router'
+import store from '@/store'
+// 路由前置守卫
+const whiteList = ['/login', '/404']
+router.beforeEach((to, from, next) => {
+  const token = store.state.user.token
+//   判断是否登录
+  if (token) {
+    if (to.path === '/login') {  //如果登录了 不能进入登录页
+      next('/')
+    } else {
+      next()  //可以进入其他页面
+    }
+  } else {     //没有登录
+    const isCludes = whiteList.includes(to.path)  // 判断进入的页面是否在白名单内
+    if (isCludes) {
+      next()               //在白名单内 直接进去
+    } else {
+      next('/login')      //不在白名单内 直接跳转到登录页面
+    }
+  }
+})
--- src/store/index.js
+++ src/store/index.js
@@ -4,16 +4,28 @@ import getters from './getters'
 import app from './modules/app'
 import settings from './modules/settings'
 import user from './modules/user'
-
+import persistedState from 'vuex-persistedstate'
 Vue.use(Vuex)
 
 const store = new Vuex.Store({
   modules: {
     app,
     settings,
-    user
+    user,
   },
-  getters
+  getters,
+  plugins: [
+    persistedState({
+      reducer(state) {
+        return {
+          // return 出来的就是要存到localstorage里面的
+          user: {
+            token: state.user.token,
+          },
+        }
+      },
+    }),
+  ],
 })
 
 export default store
